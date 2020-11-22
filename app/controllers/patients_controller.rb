863251f903daf6d2c3b94b0568861535faf23ed6
aribakhan97 <akhan41@binghamton.edu>
2020-11-22 13:50:05 -0500

    add new create

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,14 @@
 class PatientsController < ApplicationController
+
+    def create
+        patient = Patient.create(patient_params)
+        render json: patient
+    end
+
+    private
+
+    def patient_params
+        params.require(:patient).permit!
+    end
+    
 end
