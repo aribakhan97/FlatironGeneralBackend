a69fb34418f68bb4f053dd6a8b7a5ebd477cbf9c
aribakhan97 <akhan41@binghamton.edu>
2020-11-23 23:18:48 -0500

    added more patients

--- db/seeds.rb
+++ db/seeds.rb
@@ -113,4 +113,99 @@ profile5={
 Patient.create!(name: 'Richard Webber', profile: profile5.to_json, comments: 'none', has_covid: true, office_id: Office.first.id, age: 59, gender: 'Male', floor: 'Floor 1 Room 103', reason_for_visit: 'Difficulty Breathing', priority: false  )
 
 
+profile6={
+  current_medications: ['Imitrex'], 
+  medical_conditions: ['Migraines'],
+  allergies: ['Ibuprofen'],
+  surgeries: ['None'],
+  history: ['Dad has HTN.'],
+  smoking: false,
+  vitals: {
+    height: 62,
+    weight: 122,
+    temperature: 98.9,
+    oxygen: 98,
+    bp: '138/88',
+    hr: 81,
+    rr: 17,
+  }
+}
+Patient.create!(name: 'April Kepner', profile: profile6.to_json, comments: 'none', has_covid: false, office_id: Office.first.id, age: 29, gender: 'Female', floor: 'Floor 2 Room 208', reason_for_visit: 'Left Ankle Pain', priority: false )
 
+profile7={
+  current_medications: ['Sertraline'], 
+  medical_conditions: ['PTSD'],
+  allergies: ['Penicillin'],
+  surgeries: ['Colonoscopy'],
+  history: ['Mom and Dad both has HTN and DM'],
+  smoking: false,
+  vitals: {
+    height: 72,
+    weight: 215,
+    temperature: 98.9,
+    oxygen: 97,
+    bp: '112/78',
+    hr: 67,
+    rr: 16,
+  }
+}
+Patient.create!(name: 'Owen Hunt', profile: profile7.to_json, comments: 'none', has_covid: true, office_id: Office.first.id, age: 42, gender: 'Male', floor: 'Floor 1 Room 113', reason_for_visit: 'Chest Pain', priority: false )
+
+
+
+profile8={
+  current_medications: ['None'], 
+  medical_conditions: ['Anxiety'],
+  allergies: ['Sulda Drugs'],
+  surgeries: ['Tonsillectomy'],
+  history: ['Dad has DM'],
+  smoking: false,
+  vitals: {
+    height: 68,
+    weight: 163,
+    temperature: 99.1,
+    oxygen: 100,
+    bp: '120/84',
+    hr: 82,
+    rr: 16,
+  }
+}
+Patient.create!(name: 'George OMalley', profile: profile8.to_json, comments: 'none', has_covid: false, office_id: Office.first.id, age: 30, gender: 'Male', floor: 'Floor 2 Room 216', reason_for_visit: 'Eye Pain', priority: false )
+
+profile9={
+  current_medications: ['Glipizide'], 
+  medical_conditions: ['Type II Diabetes'],
+  allergies: ['None'],
+  surgeries: ['Brain Surgery for Tumor'],
+  history: ['Brain Tumor'],
+  smoking: false,
+  vitals: {
+    height: 62,
+    weight: 110,
+    temperature: 98.3,
+    oxygen: 99,
+    bp: '114/81',
+    hr: 79,
+    rr: 16,
+  }
+}
+Patient.create!(name: 'Amelia Shepherd', profile: profile9.to_json, comments: 'none', has_covid: true, office_id: Office.first.id, age: 35, gender: 'Female', floor: 'Floor 1 Room 101', reason_for_visit: 'Allergic Reaction', priority: false )
+
+profile10={
+  current_medications: ['None'], 
+  medical_conditions: ['Anxiety'],
+  allergies: ['None'],
+  surgeries: ['GSW to the left leg in 2012'],
+  history: ['No History'],
+  smoking: false,
+  vitals: {
+    height: 69,
+    weight: 132,
+    temperature: 98.4,
+    oxygen: 99,
+    bp: '113/71',
+    hr: 69,
+    rr: 16,
+  }
+}
+Patient.create!(name: 'Teddy Altman', profile: profile10.to_json, comments: 'none', has_covid: false, office_id: Office.first.id, age: 32, gender: 'Female', floor: 'Floor 2 Room 214', reason_for_visit: 'Nausea', priority: false )
